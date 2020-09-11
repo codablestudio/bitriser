c40612efb218d77efce18362b0d3949550000777
Filip Pranklin <filip.pranklin@gmail.com>
2020-09-11 11:44:38 +0200

    fix scrolling

--- app/src/main/java/studio/codable/bitriser/view/custom/ListWithDividers.kt
+++ app/src/main/java/studio/codable/bitriser/view/custom/ListWithDividers.kt
@@ -1,8 +1,8 @@
 package studio.codable.bitriser.view.custom
 
+import androidx.compose.foundation.ScrollableColumn
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumnForIndexed
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -16,14 +16,16 @@ fun <T> ListWithDividers(
     onItemClick: (T) -> Unit,
     child: @Composable() (modifier: Modifier, index: Int, item: T) -> Unit
 ) {
-    LazyColumnForIndexed(modifier = modifier, items = itemList) { index, item ->
-        child(modifier.clickable(onClick = { onItemClick(item) }), index, item)
+    ScrollableColumn(modifier = modifier) {
+        itemList.forEachIndexed { index, item ->
+            child(modifier.clickable(onClick = { onItemClick(item) }), index, item)
 
-        if (index != itemList.size - 1) {
-            Divider(
-                modifier = modifier.padding(horizontal = 30.dp),
-                color = MaterialTheme.colors.primary
-            )
+            if (index != itemList.size - 1) {
+                Divider(
+                    modifier = modifier.padding(horizontal = 30.dp),
+                    color = MaterialTheme.colors.primary
+                )
+            }
         }
     }
 }
